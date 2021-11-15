ea8c3c9069b5e185d76418d740129f91714c8767
Ian_dl <835128236@qq.com>
2021-11-15 10:29:46 +0800

    2021-11-15-1029

--- admin/controllers/cstuinfoController.php
+++ admin/controllers/cstuinfoController.php
@@ -1,4 +1,3 @@
-
 <?php
 
 class cstuinfoController extends BaseController {
@@ -15,10 +14,7 @@ class cstuinfoController extends BaseController {
         set_cookie('_currentUrl_', Yii::app()->request->url);
         $modelName = $this->model;
         $model = $modelName::model();
-<<<<<<< HEAD
         //$this->open();
-=======
->>>>>>> 0f395328a68d62640194328c33a193c847640f7f
         $criteria = new CDbCriteria;
         $styear=="-1"?$styear=base_year::model()->now():"";
         $sterm=="-1"?$sterm=base_term::model()->now():"";
@@ -26,12 +22,8 @@ class cstuinfoController extends BaseController {
         if($sterm!="-1") $model->courseterm = $sterm;
         $w1=get_where('1=1',$styear,'courseyear',$styear,'"');
         //put_msg("19"." ".$w1);
-<<<<<<< HEAD
         $criteria->condition=get_where($w1,$sterm,'courseterm',$sterm,'"');
 
-=======
-        $criteria->condition=get_where($w1,$sterm,'cterm',$sterm,'"');
->>>>>>> 0f395328a68d62640194328c33a193c847640f7f
         //put_msg("21"." ".$criteria->condition);
         /*criteria为筛选条件，更改对条件即可完成筛选，第一个不用改，第二个改成index里面对应命名
         （即参数，应设置为默认0），第三个为此模块中的筛选的表名，第四个为index里面对应命名（即参数）*/
@@ -101,7 +93,6 @@ class cstuinfoController extends BaseController {
         
     }
 
-<<<<<<< HEAD
     public function open($excelFile = "")
     {
 
@@ -134,7 +125,4 @@ class cstuinfoController extends BaseController {
                             }
 
     }
-=======
-
->>>>>>> 0f395328a68d62640194328c33a193c847640f7f
 }
